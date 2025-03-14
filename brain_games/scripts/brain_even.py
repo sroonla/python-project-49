50e254c5edde3bece40948e76d2cbcb3470aa1a8
sroonla <sr0.onlady@mail.ru>
2025-03-15 03:19:21 +0900

    Исправлен файл brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,7 +2,6 @@ from brain_games.engine import run_game
 import random
 
 def get_question_and_answer():
-    """Генерирует число и определяет, чётное оно или нет."""
     number = random.randint(1, 100)
     question = str(number)
     correct_answer = "yes" if number % 2 == 0 else "no"
@@ -10,7 +9,6 @@ def get_question_and_answer():
     return question, correct_answer
 
 def main():
-    """Запуск игры "Проверка на четность"."""
     run_game(get_question_and_answer)
 
 if __name__ == "__main__":
